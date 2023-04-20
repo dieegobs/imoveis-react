ca52240463125e73ac38bdf293a148862dd91e1e
dieegobs <diegohevant@gmail.com>
2023-04-19 23:26:18 -0300

    Style do container e da logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import LogoImg from '../../assets/logo.png'
 import { Container, Logo, Menu } from './styles'
 
 const Header = () => {
   return (
     <Container>
         <Logo>
-          <img src="" alt="" />
+          <img src={LogoImg} alt="" />
         </Logo>
         <Menu>
           <ul>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,14 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     padding: 25px 150px;
-    height: 56px;
+    height: 96px;
     display: flex;
     justify-content: space-between;
     align-items: center;
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    background-color: var(--white);
 `
 
 export const Logo = styled.div`
-    
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    img{
+        width: 300px;
+    }
 
 `
 
