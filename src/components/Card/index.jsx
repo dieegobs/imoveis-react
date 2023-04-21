9be0eb14506fda0c6ab6e9bdd8195c7e7e31483b
dieegobs <diegohevant@gmail.com>
2023-04-20 23:03:28 -0300

    Fazendo a parte do card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container,Img, Description, Title } from './styles'
+
+export default function index() {
+  return (
+   <Container>
+    <Img>
+        <img src="" alt="" />
+    </Img>
+    <Description>
+        <Title>Apartamento</Title>
+    </Description>
+   </Container>
+  )
+}
---
+++ src/components/Card/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+ 
+   
+`
+
+export const Img = styled.div`
+   
+   
+`
+
+export const Description = styled.div`
+   
+   
+`
+export const Itens = styled.div`
+   
+   
+`
